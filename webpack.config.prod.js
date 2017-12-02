3e2d140678b4a6581feb7afe1e3fec2894fa6e70
elles <e113s10@gmail.com>
2017-12-01 23:39:39 -0600

    Production

---
+++ webpack.config.prod.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+module.exports = {
+    entry: __dirname + "/src/index.js",
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js",
+        publicPath: "./"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react'],
+                    plugins: ['react-hot-loader/babel','transform-class-properties']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+    ],
+    devServer: {
+        contentBase: "./public",
+        historyApiFallback: true,
+        inline: true,
+        hot: true
+    }
+};
