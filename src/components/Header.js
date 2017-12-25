3f96c058773652889c064fd0cf70813a21dbaf64
elles <e113s10@gmail.com>
2017-12-25 11:30:12 -0600

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+const Header = () => {
+    return(
+        <div id="Header">
+            <img src="./assets/icon.png" alt="Logo"/>
+            <h1>Chat-eando</h1>
+        </div>
+    );
+};
+
+export default Header;
