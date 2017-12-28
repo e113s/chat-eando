00cda41063aca63b38d295bd8f9f8b0b29cd042c
elles <e113s10@gmail.com>
2017-12-28 14:37:09 -0600

    Connection with firebase for user registration

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Header from './Header';
 
 class LoginContainer extends Component {
-    state = { email: '', password: ''};
+    state = { email: '', password: '', error:''};
 
     handleEmailChange = (event) => {
         this.setState({ email: event.target.value });
@@ -14,27 +14,64 @@ class LoginContainer extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state);
+        this.setState({ error: '' });
+        if(this.state.email && this.state.password){
+            this.login();
+        }else{
+            this.setState({ error: 'Complete ambos campos por favor'});
+        }
     };
+
+    login() {
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                if(err.code === 'auth/user-not-found'){
+                    this.signup();
+                } else {
+                    this.setState({ error: 'Error al iniciar sesión.'});
+                }
+            })
+    }
+
+    signup() {
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+            this.setState({ error: 'Error al Registrarse.'});
+        })
+    }
     
     render(){
         return(
             <div id="LoginContainer" className="inner-container">
                 <Header />
                 <form onSubmit = {this.handleSubmit}>
-                    <p>Sign in or Sing up by entering your email and password.</p>
+                    <p>Inicie sesión o registrese ingresando su correo electrónico y contraseña.</p>
                     <input
                         type="text"
                         onChange={this.handleEmailChange}
                         value={this.state.email}
-                        placeholder="Your email"
+                        placeholder="Su correo"
                         />
                     <input
                         type="password"
                         onChange={this.handlePasswordChange}
                         value={this.state.password}
-                        placeholder="Your password"/>
-                    <button className="red light" type="submit">Login</button>
+                        placeholder="Su contraseña"/>
+
+                    <p className = "error">{this.state.error}</p>
+
+                    <button className="red light" type="submit">Iniciar Sesion</button>
                 </form>
             </div>
         );
