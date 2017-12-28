6a82de4bc2508d911be1c95db1701b1d5e7b88c8
elles <e113s10@gmail.com>
2017-12-28 14:35:58 -0600

    Authentication with Firebase

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,20 @@ import React, {Component} from 'react';
 import './app.css';
 
 class App extends Component{
+    state = { user: null};
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if(user){
+                this.setState({ user });
+            }
+        });
+    }
+
+
     render(){
         return(
-            <div id="container" className="inner-container">
+            <div id="container">
                 <LoginContainer/>
             </div>
         );
