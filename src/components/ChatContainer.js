3e49728c3f9d8b8f8fe771ab829f9f2d28386f08
Eder González <e113s10@gmail.com>
2017-12-30 23:38:23 -0600

    Event to send message to chat

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -2,6 +2,23 @@ import React, {Component} from 'react';
 import Header from './Header';
 
 export default class ChatContainer extends Component{
+    state = { newMessage: ''};
+
+    handleInputChange = e => {
+        this.setState({ newMessage: e.target.value });
+      };
+    
+      handleSubmit = () => {
+        this.props.onSubmit(this.state.newMessage);
+        this.setState({ newMessage: '' });
+      };
+    
+      handleKeyDown = e => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          this.handleSubmit();
+        }
+    };
 
     handleLogout = () => {
         firebase.auth().signOut();
@@ -9,11 +26,32 @@ export default class ChatContainer extends Component{
 
     render(){
         return(
-            <div id="ChatContainer">
+            <div id="ChatContainer" className="inner-container">
                 <Header>
                     <button className = "red" onClick={this.handleLogout}>Cerrar Sesión</button>
                 </Header>
-                <h1>Hello from chat container!</h1>
+                
+                <div id="message-container">
+                {this.props.messages.map(msg => (
+                    <div key={msg.id} className="message">
+                        <p>{msg.msg}</p>
+                    </div>
+                ))}
+                </div>
+
+                <div id="chat-input">
+                    <textarea 
+                        placeholder="Agrega tu mensaje..."
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleKeyDown}
+                        value={this.state.newMessage}
+                    />
+                    <button onClick={this.handleSubmit}>
+                        <svg viewBox="0 0 24 24">
+                            <path fill="#424242" d="M2,21L23,12L2,3V10L17,12L2,14V21Z"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
         );
     }
