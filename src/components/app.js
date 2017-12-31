7d10d635474811990d54d4e80a764058f01dd35a
Eder González <e113s10@gmail.com>
2017-12-30 23:37:35 -0600

    Added Firebase DB

--- src/components/app.js
+++ src/components/app.js
@@ -17,14 +17,50 @@ class App extends Component{
                 this.props.history.push('/login');
             }
         });
+
+        firebase
+            .database()
+            .ref('/messages')
+            .on('value', snapshot => {
+                this.onMessage(snapshot);
+            });
     }
 
+    onMessage = snapshot => {
+        const messages = Object.keys(snapshot.val()).map(key => {
+            const msg = snapshot.val()[key];
+            msg.id = key;
+            return msg;
+          });
+        this.setState({ messages });
+    };
+
+    handleSubmitMessage = msg => {
+        const data = {
+            msg,
+            autor: this.state.user.email,
+            user_id: this.state.user.uid,
+            timestamp: Date.now()
+        };
+        
+        firebase
+            .database()
+            .ref('messages/')
+            .push(data);
+    };
 
     render(){
         return(
             <div id="container">
                 <Route path = "/login"  component = {LoginContainer}/>
-                <Route exact path = "/" component = {ChatContainer} />
+                <Route 
+                    exact 
+                    path = "/" 
+                    render = {() => <ChatContainer 
+                                        onSubmit={this.handleSubmitMessage}
+                                        messages={this.state.messages}
+                                    />}
+                />
                 <Route path = "/users/:id" component = {UserContainer}/>
             </div>
         );
